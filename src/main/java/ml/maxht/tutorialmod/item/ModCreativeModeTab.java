33a35f82e4b6596d346c9fd118decbe4cc5e1680
Max H <maxplaysbest@gmail.com>
2022-08-24 18:06:07 +1200

    Made Tab

---
+++ src/main/java/ml/maxht/tutorialmod/item/ModCreativeModeTab.java
@@ -0,0 +1,16 @@
+package ml.maxht.tutorialmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab TUTORIAL_TAB =
+            new CreativeModeTab("tutorialmodtab") {
+                @Override
+                public ItemStack makeIcon() {
+
+
+                    return new ItemStack(ModItems.ZIRCON.get());
+                }
+            };
+}
--- src/main/java/ml/maxht/tutorialmod/item/ModItems.java
+++ src/main/java/ml/maxht/tutorialmod/item/ModItems.java
@@ -1,10 +1,7 @@
 package ml.maxht.tutorialmod.item;
 
 import ml.maxht.tutorialmod.TutorialMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraftforge.eventbus.EventBus;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,7 +12,9 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MODID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
